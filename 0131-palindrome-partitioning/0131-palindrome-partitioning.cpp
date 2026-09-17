f09d5e0fd6ece65450da2fab45b6f9034de25515
Maansi Gaur <maansigaur36@gmail.com>
2026-09-17 21:05:48 +0530

    Time: 32 ms (78.97%), Space: 53.5 MB (83.38%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+
+    bool isPalindrome(string &s, int start, int end) {
+        while (start < end) {
+            if (s[start] != s[end])
+                return false;
+
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+
+    void solve(string &s, int index,
+               vector<string> &path,
+               vector<vector<string>> &ans) {
+
+        // String completely partition ho gayi
+        if (index == s.size()) {
+            ans.push_back(path);
+            return;
+        }
+
+        // index se different endings try karo
+        for (int i = index; i < s.size(); i++) {
+
+            // Sirf palindrome substring choose karenge
+            if (isPalindrome(s, index, i)) {
+                                        
+                path.push_back(s.substr(index, i - index + 1));
+
+                solve(s, i + 1, path, ans);
+
+                // Backtracking
+                path.pop_back();
+            }
+        }
+    }
+
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> ans;
+        vector<string> path;
+
+        solve(s, 0, path, ans);
+
+        return ans;
+    }
+};
