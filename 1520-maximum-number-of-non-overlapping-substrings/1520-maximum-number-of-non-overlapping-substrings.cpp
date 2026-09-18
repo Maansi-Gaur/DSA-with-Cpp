f125da2f9da076f2ca81640d2a6ee086702ca784
Maansi Gaur <maansigaur36@gmail.com>
2026-09-18 12:41:18 +0530

    Time: 8 ms (78.98%), Space: 23.2 MB (88.07%) - LeetHub

---
+++ 1520-maximum-number-of-non-overlapping-substrings/1520-maximum-number-of-non-overlapping-substrings.cpp
@@ -0,0 +1,70 @@
+class Solution {
+public:
+    vector<string> maxNumOfSubstrings(string s) {
+
+        int n = s.size();
+
+        // First and last occurrence of every character
+        vector<int> first(26, n);
+        vector<int> last(26, -1);
+
+        for (int i = 0; i < n; i++) {
+            int c = s[i] - 'a';
+            first[c] = min(first[c], i);
+            last[c] = i;
+        }
+
+        vector<pair<int, int>> intervals;
+
+        // Try to create a valid interval starting from
+        // the first occurrence of each character
+        for (int c = 0; c < 26; c++) {
+
+            if (last[c] == -1)
+                continue;
+
+            int l = first[c];
+            int r = last[c];
+
+            bool valid = true;
+
+            for (int i = l; i <= r; i++) {
+
+                int x = s[i] - 'a';
+
+                // This character appeared before l,
+                // so we cannot make a valid substring
+                if (first[x] < l) {
+                    valid = false;
+                    break;
+                }
+
+                // We must include all occurrences of this character
+                r = max(r, last[x]);
+            }
+
+            if (valid)
+                intervals.push_back({l, r});
+        }
+
+        // Greedy: choose intervals ending earliest
+        sort(intervals.begin(), intervals.end(),
+             [](auto &a, auto &b) {
+                 return a.second < b.second;
+             });
+
+        vector<string> ans;
+
+        int prevEnd = -1;
+
+        for (auto &[l, r] : intervals) {
+
+            if (l > prevEnd) {
+                ans.push_back(s.substr(l, r - l + 1));
+                prevEnd = r;
+            }
+        }
+
+        return ans;
+    }
+};
