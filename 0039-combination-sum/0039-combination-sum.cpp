ae4c0ce01887a60321a7160ea52928cba20c609c
Maansi Gaur <maansigaur36@gmail.com>
2026-09-18 22:11:51 +0530

    Time: 0 ms (100%), Space: 14.1 MB (57.5%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,29 +1,38 @@
 class Solution {
 public:
-      set <vector<int>> st;
- void solve(vector<int>& candidates, int target, int idx, vector<int>& combi,vector<vector<int>>& ans) {
-       
+    void solve(vector<int>& candidates, int target, int idx,
+               vector<int>& combi, vector<vector<int>>& ans) {
 
-        if(target<0|| idx==candidates.size()) return;
-
-        if(target==0){
-            if(st.find(combi)==st.end()){
-                ans.push_back(combi);
-                st.insert(combi);
-                return;
-            }
+        if (target == 0) {
+            ans.push_back(combi);
+            return;
         }
-        
-        combi.push_back(candidates[idx]);
-        solve(candidates,target-candidates[idx],idx+1,combi,ans);
-        solve(candidates,target-candidates[idx],idx,combi,ans);
-        combi.pop_back();
-        solve(candidates,target,idx+1,combi,ans);
+
+        for (int i = idx; i < candidates.size(); i++) {
+
+            if (candidates[i] > target)
+                break;
+
+            combi.push_back(candidates[i]);
+
+            // i, not i+1
+            // because same number can be used again
+            solve(candidates, target - candidates[i],
+                  i, combi, ans);
+
+            combi.pop_back();
+        }
     }
+
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+
+        sort(candidates.begin(), candidates.end());
+
         vector<vector<int>> ans;
         vector<int> combi;
-        solve(candidates,target,0,combi,ans);
+
+        solve(candidates, target, 0, combi, ans);
+
         return ans;
     }
 };
