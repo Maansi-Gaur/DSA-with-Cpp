d542d8a6f30d6befbc527d6bda54908c27836611
Maansi Gaur <maansigaur36@gmail.com>
2026-09-22 19:22:41 +0530

    Time: 331 ms (47.27%), Space: 298.7 MB (52.73%) - LeetHub

---
+++ 3525-find-x-value-of-array-ii/3525-find-x-value-of-array-ii.cpp
@@ -0,0 +1,155 @@
+class Solution {
+public:
+
+    struct Node {
+        int prod;
+        int cnt[5];
+
+        Node() {
+            prod = 1;
+            for (int i = 0; i < 5; i++)
+                cnt[i] = 0;
+        }
+    };
+
+    int n, k;
+    vector<Node> seg;
+
+    Node mergeNode(const Node &left, const Node &right) {
+
+        Node res;
+
+        res.prod = (left.prod * right.prod) % k;
+
+        // Prefixes completely inside left
+        for (int r = 0; r < k; r++) {
+            res.cnt[r] += left.cnt[r];
+        }
+
+        // Prefixes which contain all of left
+        // and some prefix of right
+        for (int r = 0; r < k; r++) {
+            int rem = (left.prod * r) % k;
+            res.cnt[rem] += right.cnt[r];
+        }
+
+        return res;
+    }
+
+    void build(int node, int l, int r,
+               vector<int>& nums) {
+
+        if (l == r) {
+
+            int rem = nums[l] % k;
+
+            seg[node].prod = rem;
+            seg[node].cnt[rem] = 1;
+
+            return;
+        }
+
+        int mid = (l + r) / 2;
+
+        build(node * 2, l, mid, nums);
+        build(node * 2 + 1, mid + 1, r, nums);
+
+        seg[node] =
+            mergeNode(seg[node * 2],
+                      seg[node * 2 + 1]);
+    }
+
+    void update(int node, int l, int r,
+                int idx, int value) {
+
+        if (l == r) {
+
+            // Reset node
+            seg[node] = Node();
+
+            int rem = value % k;
+
+            seg[node].prod = rem;
+            seg[node].cnt[rem] = 1;
+
+            return;
+        }
+
+        int mid = (l + r) / 2;
+
+        if (idx <= mid) {
+            update(node * 2, l, mid, idx, value);
+        }
+        else {
+            update(node * 2 + 1, mid + 1, r, idx, value);
+        }
+
+        seg[node] =
+            mergeNode(seg[node * 2],
+                      seg[node * 2 + 1]);
+    }
+
+    Node query(int node, int l, int r,
+               int ql, int qr) {
+
+        if (ql <= l && r <= qr) {
+            return seg[node];
+        }
+
+        int mid = (l + r) / 2;
+
+        if (qr <= mid) {
+            return query(node * 2, l, mid, ql, qr);
+        }
+
+        if (ql > mid) {
+            return query(node * 2 + 1, mid + 1, r, ql, qr);
+        }
+
+        Node left =
+            query(node * 2, l, mid, ql, qr);
+
+        Node right =
+            query(node * 2 + 1, mid + 1, r, ql, qr);
+
+        return mergeNode(left, right);
+    }
+
+    vector<int> resultArray(
+        vector<int>& nums,
+        int k,
+        vector<vector<int>>& queries
+    ) {
+
+        this->k = k;
+        n = nums.size();
+
+        seg.resize(4 * n + 5);
+
+        build(1, 0, n - 1, nums);
+
+        vector<int> ans;
+        ans.reserve(queries.size());
+
+        for (auto &q : queries) {
+
+            int index = q[0];
+            int value = q[1];
+            int start = q[2];
+            int x = q[3];
+
+            // Update persists
+            update(1, 0, n - 1,
+                   index, value);
+
+            // Get information for [start, n-1]
+            Node res =
+                query(1, 0, n - 1,
+                      start, n - 1);
+
+            ans.push_back(res.cnt[x]);
+        }
+
+        return ans;
+    }
+};
