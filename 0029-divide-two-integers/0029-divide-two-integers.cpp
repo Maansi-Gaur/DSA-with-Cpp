3591c637000e502cd74b3fd18d3219240aa69f73
Maansi Gaur <maansigaur36@gmail.com>
2026-09-26 17:46:33 +0530

    Time: 0 ms (100%), Space: 8.6 MB (72.67%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -11,11 +11,12 @@ public:
         long long ans = 0;
 
         while (n >= d) {
-
+            //temp = divisor ka kitna bada chunk subtract kar rahe hain
             long long temp = d;
+            //cnt = us chunk mein divisor kitni baar hai
             long long cnt = 1;
 
-            while (n >= (temp << 1)) {
+            while (n >= (temp << 1)) {//<< 1 means multiply by 2.
                 temp <<= 1;
                 cnt <<= 1;
             }
@@ -24,7 +25,7 @@ public:
             ans += cnt;
         }
 
-        if ((dividend < 0) ^ (divisor < 0))
+        if ((dividend < 0) ^ (divisor < 0))//Agar exactly ek number negative hai, answer negative hoga.
             ans = -ans;
 
         if (ans > INT_MAX)
